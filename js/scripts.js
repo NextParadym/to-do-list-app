a952b67d4e527b7295323a72b6bab687063f336c
Dr. Lawrence Ibeh <drlawrenceibeh@gmail.com>
2021-09-25 12:34:21 +0200

    Updated scrpts-js

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,33 @@
 
 function newItem(){
 
-//1. Adding a new item to the list:
-  let li = $('<li></li>');
-  let inputValue = $('#input').val();
-  li.append(inputValue);
-
+  //1. Adding a new item to the list:
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
   
-//2. Crossing an item out:
-  function crossOut() {
-		li.toggleClass("strike");
-	}
-
-	li.on("dblclick", function crossOut() {
-		li.toggleClass("strike");
-	});
-
-
-//3. Adding a delete button
-  let  deleteButton = $('< deleteButton></deleteButton>');
-  deleteButton.append(document.createTextNode('X'));
-  li.append(deleteButton);
-
-  deleteButton.on("click", deleteListItem);
-  function deleteListItem(){
-  li.addClass("delete")
+    
+  //2. Crossing an item out:
+    function crossOut() {
+      li.toggleClass("strike");
+    }
+  
+    li.on("dblclick", function crossOut() {
+      li.toggleClass("strike");
+    });
+  
+  
+  //3. Adding a delete button
+    let  deleteButton = $('< deleteButton></deleteButton>');
+    deleteButton.append(document.createTextNode('X'));
+    li.append(deleteButton);
+  
+    deleteButton.on("click", deleteListItem);
+    function deleteListItem(){
+    li.addClass("delete")
+    }
+  
+   // 4. Reordering the items: 
+    $('#list').sortable();
   }
-
- // 4. Reordering the items: 
-  $('#list').sortable();
-}
+  
